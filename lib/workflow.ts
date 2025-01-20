823fbedfa1c1f4c3e738ca598020fb8cd61d094e
antareephasan <antareephasan@gmail.com>
2025-01-20 19:28:30 +0600

    fix email domain

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "University Library <antareephasan@gmail.com>",
+      from: "onboarding@resend.dev",
       to: [email],
       subject,
       html: message,
