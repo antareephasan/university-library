31e0be3524cb1b87d32e72c3cfefe41af4374c5e
antareephasan <antareephasan@gmail.com>
2025-01-20 19:39:44 +0600

    fix: wrokflow sleep time

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -48,7 +48,7 @@ export const { POST } = serve<InitialData>(async (context) => {
     });
   });
 
-  await context.sleep("wait-for-3-days", THIRTY_DAYS_IN_MS);
+  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
 
   while (true) {
     const state = await context.run("check-user-state", async () => {
@@ -71,6 +71,6 @@ export const { POST } = serve<InitialData>(async (context) => {
       });
     }
 
-    await context.sleep("wait-for-1-month", THIRTY_DAYS_IN_MS);
+    await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
 });
